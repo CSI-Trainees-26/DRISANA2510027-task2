dda0f666ce9aa07846fc0412ea9dc1ba1acfe9ab
drisanagupta-a <drisanagupta8d@gmail.com>
2026-09-06 19:48:26 +0530

    add the task and delete it

--- task.js
+++ task.js
@@ -7,4 +7,32 @@ input.classList.add("taskinput");
 input.placeholder = "Enter task to add";
 newTask.appendChild(input);
 let todayTasks = document.querySelector(".todaytasks");
-todayTasks.appendChild(newTask);});
+todayTasks.appendChild(newTask);
+input.addEventListener("keydown", (e) => {
+if (e.key === "Enter") {
+ let taskText = input.value;
+if (taskText !== "") {
+let task = document.createElement("div");
+task.classList.add("taskrow");
+let checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+let text = document.createElement("p");
+text.textContent = taskText;
+task.appendChild(checkbox);
+task.appendChild(text);
+
+let delAdd = document.createElement("div");
+delAdd.classList.add("taskactions");
+let editBtn = document.createElement("button");
+editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+let deleteBtn = document.createElement("button");
+deleteBtn.classList.add("delete");
+deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+deleteBtn.addEventListener("click" ,( )=>{
+    task.remove();
+});
+ delAdd.appendChild(editBtn);
+ delAdd.appendChild(deleteBtn);
+ task.appendChild(delAdd);
+ todayTasks.appendChild(task);
+ newTask.remove();}}}); });
