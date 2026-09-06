4f5dc958b5843b7c853f0739771199f71f8bd62f
drisanagupta-a <drisanagupta8d@gmail.com>
2026-09-06 18:56:01 +0530

    add task  button functionality

--- task.js
+++ task.js
@@ -1,5 +1,10 @@
 let addBtn = document.querySelector(".addtask");
-
-addBtn.addEventListener("click", function()  {
-    console.log("Add  button clicked");
-});
+addBtn.addEventListener("click", () => {
+let newTask = document.createElement("div");
+newTask.classList.add("taskrow");
+let input = document.createElement("input");
+input.classList.add("taskinput");
+input.placeholder = "Enter task to add";
+newTask.appendChild(input);
+let todayTasks = document.querySelector(".todaytasks");
+todayTasks.appendChild(newTask);});
